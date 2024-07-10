699a5a5544517ff791d89636077c3806232872a4
seungryeol <opop0421@naver.com>
2024-07-10 23:08:50 +0900

    [feat] 중고거래 완료 횟수 dto

---
+++ src/dto/response/SecondhandTradeCount.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+export class SecondhandTradeCount{
+
+    private count:number;
+
+    constructor(count:number){
+        this.setCount(count);
+    }
+
+    private setCount(count:number){
+        if(count === null) throw new Error (`${__dirname} : count 값이 존재하지 않습니다.`);
+        this.count=count;
+    }
+
+    public static of (count:number){
+        return new SecondhandTradeCount(count);
+    }
+}
