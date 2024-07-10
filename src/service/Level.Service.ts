32c8e83a319731b17756a5742218eb44821e2baa
seungryeol <opop0421@naver.com>
2024-07-11 01:39:54 +0900

    [feat] 레벨 정보 조회 응용 서비스 함수 구현

--- src/service/Level.Service.ts
+++ src/service/Level.Service.ts
@@ -4,6 +4,10 @@ import { Product } from '../entity/Product.js';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { ProductRepository } from '../repository/Product.Repository.js';
 import { SecondhandTradeCount } from '../dto/response/SecondhandTradeCount.js';
+import { LevelInformation } from '../dto/response/LevelInformation.js';
+import { User } from '../entity/User.js';
+import { UserRepository } from '../repository/User.Repository.js';
+import { getLevelByScore, getLevelExperience, getMyLevelExperience } from '../util/level.js';
 
 
 
@@ -12,7 +16,8 @@ import { SecondhandTradeCount } from '../dto/response/SecondhandTradeCount.js';
 export class LevelService {
 
     constructor(
-        @InjectRepository(Product) private readonly productRepository: ProductRepository
+        @InjectRepository(Product) private readonly productRepository: ProductRepository,
+        @InjectRepository(User) private readonly userRepository: UserRepository,
     ) {}
 
 
@@ -23,8 +28,21 @@ export class LevelService {
      */
     async bringSecondhandTradeCount(userId:number):Promise<SecondhandTradeCount> {
         const secondhandTradeCount = await this.productRepository.findSecondhandTradeCount(userId);
-        console.log(secondhandTradeCount)
         return SecondhandTradeCount.of(secondhandTradeCount)
+    }
+
+    /**
+     * 유저 레벨 정보(내 레벨, 레벨 경험치, 내 레벨 경험치) 조회 함수
+     * @param userId 유저 id
+     * @returns 유저 레벨 정보 
+     */
+    async bringLevelInformation(userId:number):Promise<LevelInformation> {
+        const userData = await this.userRepository.findUserById(userId);
+        console.log(userData)
+        const myLevel = getLevelByScore(userData.getScore());
+        const levelExperience = getLevelExperience(myLevel)
+        const myLevelExperience = getMyLevelExperience(myLevel, userData.getScore());
+        return LevelInformation.of(levelExperience, myLevelExperience, myLevel);
     }
 
 
