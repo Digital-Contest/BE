4dc80363d0f42e7c7197dc8a191a31bc6adb0af3
seungryeol <opop0421@naver.com>
2024-07-12 00:00:20 +0900

    [style] 소개글 api 함수 주석처리

--- src/controller/Introduce.Controller.ts
+++ src/controller/Introduce.Controller.ts
@@ -20,6 +20,13 @@ export class IntroduceController{
         private readonly introduceService:IntroduceService
     ){}
 
+
+    /**
+     * 유저 및 프롬프트를 통한 소개 글 생성 함수
+     * @param introduceText 소개글 생성 시 요청 dto
+     * @param req 
+     * @returns 유저 및 프롬프트를 통한 소개 글
+     */
     @Post('/text')
     @UseBefore(compareAuthToken, uploadImage.array('files'))
     @HttpCode(200)
--- src/service/Introduce.Service.ts
+++ src/service/Introduce.Service.ts
@@ -16,7 +16,15 @@ export class IntroduceService{
 
     ) {}
 
-
+    /**
+     * chat gpt를 활용한 소개 글 생성 함수
+     * @param images 객체 이미지 url
+     * @param introduceCategory 소개 카테고리
+     * @param price 가격
+     * @param productCategory 물품 카테고리 
+     * @param product 물품
+     * @returns 소개 글
+     */
     public async makeIntroduceText(images: string[], introduceCategory:string, price:number, productCategory:string, product:string): Promise<IntroduceTextResponse> {
         this.verifyIntroduceTextCategory(getIntroduceTextCategoryByCondition(introduceCategory));
         this.verifyProductCategory(getProductCategoryByCondition(productCategory));
