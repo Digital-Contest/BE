364caa168972aefcafc4f43b6363ea6a015c89ae
seungryeol <opop0421@naver.com>
2024-07-11 12:36:11 +0900

    [fix] enum화의 table 따른 수정

--- src/repository/IntroduceTextCategoryData.Repository.ts
+++ src/repository/IntroduceTextCategoryData.Repository.ts
@@ -1,23 +1,23 @@
 import { EntityRepository, Repository } from "typeorm";
-import { IntroduceTextCategory } from "../entity/IntroduceTextCategory.js";
+//import { IntroduceTextCategory } from "../entity/IntroduceTextCategory.js";
 
 
 
-@EntityRepository(IntroduceTextCategory)
-export class IntroduceTextCategoryRepository extends Repository<IntroduceTextCategory> {
+// @EntityRepository(IntroduceTextCategory)
+// export class IntroduceTextCategoryRepository extends Repository<IntroduceTextCategory> {
 
 
-    public async findIntroduceTextCategoryByName(name:string):Promise<IntroduceTextCategory>{
-        return this.createQueryBuilder()
-            .select('c')
-            .from(IntroduceTextCategory, 'c')
-            .where('c.name = :name',{name})
-            .getOne();
-    }
+//     public async findIntroduceTextCategoryByName(name:string):Promise<IntroduceTextCategory>{
+//         return this.createQueryBuilder()
+//             .select('c')
+//             .from(IntroduceTextCategory, 'c')
+//             .where('c.name = :name',{name})
+//             .getOne();
+//     }
 
 
 
 
 
  
-}
+//}
--- src/service/Introduce.Service.ts
+++ src/service/Introduce.Service.ts
@@ -4,7 +4,7 @@ import { checkData } from "../util/checker.js";
 import { ErrorResponseDto } from "../response/ErrorResponseDto.js";
 import { ErrorCode } from "../exception/ErrorCode.js";
 //import { IntroduceTextCategory } from "../entity/IntroduceTextCategory.js";
-import { IntroduceTextCategoryRepository } from "../repository/IntroduceTextCategoryData.Repository.js";
+//import { IntroduceTextCategoryRepository } from "../repository/IntroduceTextCategoryData.Repository.js";
 import { getAllIntroduceTextCategory, getIntroduceTextCategoryByCondition } from "../util/enum/IntroduceTextCategory.js";
 
 @Service()
