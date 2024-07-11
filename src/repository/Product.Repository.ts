d1052a2487301ddf60645a1dedd5ce969258dc62
seungryeol <opop0421@naver.com>
2024-07-11 17:16:11 +0900

    [feat] 물품 조회 및 업데이트 함수

--- src/repository/Product.Repository.ts
+++ src/repository/Product.Repository.ts
@@ -18,7 +18,7 @@ export class ProductRepository extends Repository<Product> {
      * @param userId 유저 id
      * @returns 
      */
-    public findSecondhandTradeCount(userId:number):Promise<number>{
+    public async findSecondhandTradeCount(userId:number):Promise<number>{
         return this.createQueryBuilder()
             .select()
             .from(Product, 'p')
@@ -26,6 +26,35 @@ export class ProductRepository extends Repository<Product> {
             .andWhere('p.status = 1')
             .getCount();
     }
+
+    /**
+     * 물품 상태를  업데이트하는 함수  false -> 실패, true -> 성공
+     * @param userId 유저 id
+     * @param productId 물품 id
+     */
+    public async updateProductStatus(userId:number, productId:number, status:boolean){
+        return this.createQueryBuilder()
+            .update(Product)
+            .set({status:status})
+            .where('id = :productId',{productId})
+            .andWhere('user_id = :userId',{userId})
+            .execute();
+    }
+
+    /**
+     * 물품 id에 따라 물품 엔티티를 조회하는 함수
+     * @param productId 물품 id
+     * @returns 물품 엔티티
+     */
+    public async findProductById(productId:number){
+        return this.createQueryBuilder()
+            .select('p')
+            .from(Product, 'p')
+            .where('p.id = :productId',{productId})
+            .getOne();
+    }
+
+
 
 
   
