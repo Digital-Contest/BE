c033b889bfba956e10ba36312b1bc51cbfa7f210
seungryeol <opop0421@naver.com>
2024-07-12 12:28:33 +0900

    [feat] 트랜잭션 데코레이터

--- src/util/decorator/transaction.ts
+++ src/util/decorator/transaction.ts
@@ -1,13 +1,18 @@
-import { QueryRunner } from 'typeorm';
+import { Connection, QueryRunner } from 'typeorm';
 
+
+/**
+ * 트랜잭션 수행 데코레이터 함수
+ * @returns 
+ */
 function Transactional() {
     return function (target: any, propertyName: string, descriptor: PropertyDescriptor) {
         const originalMethod = descriptor.value;
         descriptor.value = async function (...args: any[]) {
-            const queryRunner: QueryRunner = this.dataSource.createQueryRunner();
+            const queryRunner: QueryRunner = this.connection.createQueryRunner(); // DataSource 대신 Connection 사용
             await queryRunner.connect();
             await queryRunner.startTransaction();
-            
+
             try {
                 const result = await originalMethod.apply(this, args);
                 await queryRunner.commitTransaction();
