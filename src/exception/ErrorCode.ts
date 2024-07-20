74f25aecb15d4653831917ab1604c0065038a823
seungryeol <opop0421@naver.com>
2024-07-21 00:31:56 +0900

    [feat] 플랫폼별 회사통계 조회 시 종류 에러추가

--- src/exception/ErrorCode.ts
+++ src/exception/ErrorCode.ts
@@ -8,7 +8,8 @@ export enum ErrorCode  {
     NOT_EXPIRED=404,
     NOT_FOUND_INTRODUCE_CATEGORY=405,
     NOT_FOUND_PRODUCT=406,
-    NOT_FOUND_PRODUCT_CATEGORY=407
+    NOT_FOUND_PRODUCT_CATEGORY=407,
+    NOT_EXIST_KIND=408
     
 }
 
@@ -20,7 +21,8 @@ const ErrorMessages: { [key: number]: string } = {
     404: "토큰이 만료되지 않았습니다.",
     405: "소개글 카테고리 데이터가 존재하지 않습니다.",
     406: "해당 물품 데이터가 존재하지 않습니다.",
-    407: "물품 카테고리 데이터가 존재하지 않습니다."
+    407: "물품 카테고리 데이터가 존재하지 않습니다.",
+    408: "선택 종류 데이터가 존재하지 않습니다."
 
 };
 
