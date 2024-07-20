c720079fd8125020b942a1bec0fcc01f7b47f245
seungryeol <opop0421@naver.com>
2024-07-21 02:19:43 +0900

    [feat] 카테고리별 말투 선호 API

--- src/controller/Satisfaction.Controller.ts
+++ src/controller/Satisfaction.Controller.ts
@@ -6,7 +6,8 @@ import { SuccessResponseDto } from "../response/SuccessResponseDto";
 import { compareAuthToken } from "../middleware/jwtMiddleware";
 import { UserNickname } from "../dto/response/UserNickname";
 import { SatisfactionService } from "../service/Satisfaction.Service";
-import { CompanySatisfaction } from "../dto/response/CompanySatisfaction";
+import { PlatformSatisfaction } from "../dto/response/PlatformSatisfaction";
+import { CategorySatisfaction } from "../dto/response/CategorySatisfaction";
 
 
 
@@ -26,10 +27,27 @@ export class SatisfactionController {
      */
     @HttpCode(200)
     @UseBefore(compareAuthToken)
-    @Get('/:kind')
-    async bringProductSatisfation(@Req() req: Request, @Param('kind') kind:string): Promise<SuccessResponseDto<CompanySatisfaction[]>> {
-        const result = await this.satisfactionService.bringProductSatisfation(req.decoded.user_id, kind);
-        console.log("물품 통계 조회 완료");
+    @Get('/platform/:kind')
+    async bringPlatfromSatisfation(@Req() req: Request, @Param('kind') kind:string): Promise<SuccessResponseDto<PlatformSatisfaction[]>> {
+        const result = await this.satisfactionService.bringPlatfromSatisfation(req.decoded.user_id, kind);
+        console.log("플랫폼별 회사통계 조회 완료");
+        return SuccessResponseDto.of(result);
+    }
+
+
+
+    /**
+     * 카테고리별 선호 말투통계 -> 전체 or 나의 통계 선택
+     * @param req 
+     * @param kind 종류 
+     * @returns 
+     */
+    @HttpCode(200)
+    @UseBefore(compareAuthToken)
+    @Get('/category/:kind')
+    async bringCategorySatisfation(@Req() req: Request, @Param('kind') kind:string): Promise<SuccessResponseDto<CategorySatisfaction[]>> {
+        const result = await this.satisfactionService.bringCategorySatisfation(req.decoded.user_id, kind);
+        console.log("카테고리별 회사통계 조회 완료");
         return SuccessResponseDto.of(result);
     }
 
--- src/service/Satisfaction.Service.ts
+++ src/service/Satisfaction.Service.ts
@@ -3,7 +3,8 @@ import { InjectRepository } from 'typeorm-typedi-extensions';
 import { ErrorResponseDto } from '../response/ErrorResponseDto';
 import { ErrorCode } from '../exception/ErrorCode';
 import { ProductRepository } from '../repository/Product.Repository';
-import { CompanySatisfaction } from '../dto/response/CompanySatisfaction';
+import { PlatformSatisfaction } from '../dto/response/PlatformSatisfaction';
+import { CategorySatisfaction } from '../dto/response/CategorySatisfaction';
 
 
 
@@ -18,25 +19,56 @@ export class SatisfactionService {
     /**
      * 전체 or 나의 플랫폼별 선호 말투 조회 함수
      * @param userId 유저 id
-     * @param kind 종류 whole -> 전체, mine -> 나의
+     * @param kind 종류 platform-whole -> 전체, platform-mine -> 나의
      * @returns 
      */
-    async bringProductSatisfation(userId:number, kind:string) {
-        const companySatisfaction = await this.bringCompanySatisfactionAccordingToKind(userId, kind);
-        return this.extractMostIntroduceCategory(companySatisfaction);
+    async bringPlatfromSatisfation(userId:number, kind:string) {
+        const platformSatisfaction = await this.bringSatisfactionAccordingToKind(userId, kind) as PlatformSatisfaction[];
+        return this.extractMostPlatform(platformSatisfaction);
+    }
+
+
+    /**
+     * 전체 or 나의 카체고리별 선호 말투 조회 함수
+     * @param userId 유저 id
+     * @param kind 종류 category-whole -> 전체,  category-mine -> 나의
+     * @returns 
+     */
+    async  bringCategorySatisfation(userId:number, kind:string) {
+         const categorySatisfaction = await this.bringSatisfactionAccordingToKind(userId, kind) as CategorySatisfaction[];
+         return this.extractMostCategory(categorySatisfaction);
+    }
+
+
+
+    /**
+     * 
+     * @param platformSatisfaction 플랫폼별 선호 말투 중 가장 인기 있는 말투를 선별하는 함수
+     * @returns 
+     */
+    public extractMostPlatform(platformSatisfaction:PlatformSatisfaction[]){ 
+        const result = platformSatisfaction.reduce((acc, current) => {
+            const existing = acc.find(item => item.getCompany() === current.getCompany());
+            if (!existing || existing.introduceTextCategoryCount < current.getIntroduceTextCategoryCount()) {
+                acc = acc.filter(item => item.getCompany() !== current.getCompany());
+                acc.push(current);
+            }
+            return acc;
+        }, []);
+        return result;
     }
 
 
     /**
      * 
-     * @param companySatisfaction 플랫폼별 선호 말투 중 가장 인기 있는 말투를 선별하는 함수
+     * @param categorySatisfaction 카테고리별 선호 말투 중 가장 인기 있는 말투를 선별하는 함수
      * @returns 
      */
-    public extractMostIntroduceCategory(companySatisfaction:CompanySatisfaction[]){ 
-        const result = companySatisfaction.reduce((acc, current) => {
-            const existing = acc.find(item => item.getCompany() === current.getCompany());
+    public extractMostCategory(categorySatisfaction:CategorySatisfaction[]){ 
+        const result = categorySatisfaction.reduce((acc, current) => {
+            const existing = acc.find(item => item.getCategory() === current.getCategory());
             if (!existing || existing.introduceTextCategoryCount < current.getIntroduceTextCategoryCount()) {
-                acc = acc.filter(item => item.getCompany() !== current.getCompany());
+                acc = acc.filter(item => item.getCategory() !== current.getCategory());
                 acc.push(current);
             }
             return acc;
@@ -51,16 +83,23 @@ export class SatisfactionService {
      * @param kind 종류
      * @returns 
      */
-    public async bringCompanySatisfactionAccordingToKind(userId:number, kind:string){
+    public async bringSatisfactionAccordingToKind(userId:number, kind:string){
         switch(true){
-            case kind === 'whole' :
-                return this.productRepository.findWholeProductSatisfaction();
-            case kind === 'mine':
-                return this.productRepository.findMineProductSatisfaction(userId);
+            case kind === 'platform-whole' :
+                return this.productRepository.findWholePlatformSatisfaction();
+            case kind === 'platform-mine':
+                return this.productRepository.findMinePlatformSatisfaction(userId);
+            case kind === 'category-whole':
+                return this.productRepository.findWholeCategorySatisfaction();
+            case kind === 'category-mine':
+                return this.productRepository.findMineCategorySatisfaction(userId);  
             default :
                 throw ErrorResponseDto.of(ErrorCode.NOT_EXIST_KIND);
         }
     }
+
+
+
 
 
 
