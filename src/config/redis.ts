b9f5c20a51264d4c86cddefe838cac464481078f
seungryeol <opop0421@naver.com>
2024-07-24 01:36:19 +0900

    [test] auth.controller 테스트 코드 작성

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -16,10 +16,6 @@ const connectToRedis = async () => {
         await redisClient.connect();
         console.log('Connected to Redis');
     } catch (error) {
-        setTimeout(() => {
-            redisClient.quit(); // 현재 연결 종료
-            redisClient.connect(); // 다시 연결 시도
-        }, 3000);
         console.error('Error connecting to Redis:');
     }
 };
--- test/controller/Auth.Controller.test.ts
+++ test/controller/Auth.Controller.test.ts
@@ -1,66 +1,67 @@
-import 'reflect-metadata';
-import 'mocha';
-import { expect } from 'chai';
-import sinon from 'sinon';
 import { AuthController } from '../../src/controller/Auth.Controller';
 import { AuthService } from '../../src/service/Auth.Service';
 import { SuccessResponseDto } from '../../src/response/SuccessResponseDto';
 import { LoginResponse } from '../../src/dto/response/loginResponse';
 import { Request, Response } from 'express';
-//import chaiAsPromised from 'chai-as-promised';
+import { Token } from '../../src/dto/response/Token';
 
 
-//chai.use(chaiAsPromised);
+jest.mock('../../src/service/Auth.Service');
+jest.mock('../../src/util/JwtManager');
+jest.mock('../../src/util/TokenManager');
+jest.mock('../../src/util/SocialLogin');
+jest.mock('../../src/repository/User.Repository');
 
-describe('AuthController', () => {
-    let authService: AuthService;
-    let authController: AuthController;
-    let req: Partial<Request>;
-    let res: Partial<Response>;
+const mockAuthService = new AuthService(
+  {} as any, // Mocked UserRepository
+  {} as any, // Mocked JwtManager
+  {} as any, // Mocked TokenManager
+  {} as any  // Mocked SocialLogin
+) as jest.Mocked<AuthService>;
 
-    beforeEach(() => {
-        authService = sinon.createStubInstance(AuthService);
-        authController = new AuthController(authService);
-        req = {};
-        res = {};
+const authController = new AuthController(mockAuthService);
+
+describe('AuthController 테스트', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('POST /auth/login/kakao', () => {
+    it('로그인 응답이 성공적으로 반환', async () => {
+
+      const mockLoginResponse: LoginResponse = LoginResponse.of('dummy-token', 'dummy-token', 'user');
+      mockAuthService.kakaoLogin.mockResolvedValue(mockLoginResponse);  // 비동기로 반환할 값을 설정함.
+
+      const req = { headers: { authorization: 'testToken' } } as unknown as Request;
+      const result = await authController.kakaoLogin(req);
+      expect(result).toEqual(SuccessResponseDto.of(mockLoginResponse)); // 기대한 값과 실제 값이 일치하는지를 검증, 객체나 배열의 경우 구조와 내용이 동일한지를 검사
+      expect(mockAuthService.kakaoLogin).toHaveBeenCalledWith('testToken');  // service 함수가 특정 인자로 호출되었는지 검증
     });
+  });
 
-    describe('kakaoLogin', () => {
-        it('should login with kakao and return a success response', async () => {
-            const loginResponse: LoginResponse = LoginResponse.of('dummy-token', 'dummy-token', 'user');
-            (authService.kakaoLogin as sinon.SinonStub).resolves(loginResponse);
+  describe('DELETE /auth/logout', () => {
+    it('로그아웃 메서드가 호출', async () => {
+      const mockUserId = '12345';
+      const req = { decoded: { user_id: mockUserId } } as unknown as Request;
+      await authController.logout(req);
 
-            req.headers = { authorization: 'Bearer dummy-auth' };
-
-            const result = await authController.kakaoLogin(req as Request);
-
-         
-            expect(result).to.deep.equal(SuccessResponseDto.of(loginResponse));
-        });
+      expect(mockAuthService.logout).toHaveBeenCalledWith(mockUserId);
+      expect({}).toEqual({});
     });
+  });
 
-    describe('logout', () => {
-        it('should logout and return a success response', async () => {
-            (req as any).decoded = { user_id: 'dummy-user-id' };
+  describe('POST /auth/token-reissue', () => {
+    it('새로운 액세스 토큰과 리프레시 토큰 반환', async () => {
 
-            const result = await authController.logout(req as Request);
+        const mockTokenResponse = Token.of('new-access-token', 'new-refresh-token');
+        mockAuthService.reissueToken.mockResolvedValue(mockTokenResponse);
 
-            expect(result).to.deep.equal(SuccessResponseDto.of());
-        });
+        const req = { headers: { authorization: 'Bearer oldAccessToken', refresh: 'oldRefreshToken' } } as unknown as Request;
+        const result = await authController.reissueToken(req);
+
+        expect(mockAuthService.reissueToken).toHaveBeenCalledWith('Bearer oldAccessToken', 'oldRefreshToken');
+        expect(result).toEqual(SuccessResponseDto.of(mockTokenResponse));
+
     });
-
-    describe('reissueToken', () => {
-        it('should reissue token and return a success response', async () => {
-            const tokenResponse = { accessToken: 'new-access-token', refreshToken: 'new-refresh-token' };
-            (authService.reissueToken as sinon.SinonStub).resolves(tokenResponse);
-
-            req.headers = {
-                authorization: 'Bearer old-access-token',
-                refresh: 'old-refresh-token'
-            };
-
-            const result = await authController.reissueToken(req as Request);
-            expect(result).to.deep.equal(SuccessResponseDto.of(tokenResponse));
-        });
-    });
+  });
 });
