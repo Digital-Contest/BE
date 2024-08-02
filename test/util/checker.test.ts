e476c96a375604d4be3fa48e535d73d884616d90
seungryeol <opop0421@naver.com>
2024-08-03 03:34:52 +0900

    [test] checker

--- test/util/checker.test.ts
+++ test/util/checker.test.ts
@@ -1,13 +1,17 @@
 import {checkData} from '../../src/util/checker'
 
 describe('checker 테스트', ()=>{
-
-
-    it('checkData 함수 테스트', ()=>{
+    it('checkData data undefined', ()=>{
         const data:any = undefined;
+        const checkResult = false;
+        const result = checkData(data);
+        expect(result).toEqual(checkResult);
+    });
+
+    it('checkData data 존재', ()=>{
+        const data:any = 'data';
         const checkResult = true;
         const result = checkData(data);
-
-        expect(result).not.toEqual(checkResult);
+        expect(result).toEqual(checkResult);
     });
 });
