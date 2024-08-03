825f177b94a71c5894319afd38149b1fb937819b
seungryeol <opop0421@naver.com>
2024-08-04 01:35:03 +0900

    [test] TokenManager 테스트

---
+++ test/util/TokenManager.test.ts
@@ -0,0 +1,61 @@
+import { TokenManager } from "../../src/util/TokenManager";
+import { redisClient } from "../../src/config/redis";
+import { RedisClientType } from "redis";
+
+
+jest.mock('../../src/config/redis', () => ({
+    redisClient: {
+        v4: {
+            set: jest.fn(),
+            del: jest.fn(),
+            get: jest.fn(),
+        },
+        quit: jest.fn(),
+        on: jest.fn(),
+    },
+}));
+
+describe('TokenManager 테스트', () => {
+
+    let tokenManager: TokenManager;
+    beforeEach(() => {
+        tokenManager = new TokenManager();
+        jest.clearAllMocks(); 
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks(); 
+    });
+
+    describe('setToken 테스트', () => {
+        it('setToken 정상처리', async () => {
+            const key = "testKey";
+            const value = "testValue";
+            await tokenManager.setToken(key, value);
+            expect(redisClient.v4.set).toHaveBeenCalledWith(key, value);
+            expect(redisClient.v4.set).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('deleteToken 테스트', () => {
+        it('deleteToken 정상처리', async () => {
+            const key = "testKey";
+            await tokenManager.deleteToken(key);
+            expect(redisClient.v4.del).toHaveBeenCalledWith(key);
+            expect(redisClient.v4.del).toHaveBeenCalledTimes(1); 
+        });
+    });
+
+
+    describe('getToke 테스트', () => {
+        it('getToke 정상처리', async () => {
+            const key = "testKey";
+            await tokenManager.getToken(key);
+            expect(redisClient.v4.get).toHaveBeenCalledWith(key);
+            expect(redisClient.v4.get).toHaveBeenCalledTimes(1); 
+        });
+    });
+
+
+
+});
