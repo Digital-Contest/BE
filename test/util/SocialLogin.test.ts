560a45a9714d5e56c615936f09360240416421c1
seungryeol <opop0421@naver.com>
2024-08-05 03:01:41 +0900

    [test] SocialLogin 완료

--- test/util/SocialLogin.test.ts
+++ test/util/SocialLogin.test.ts
@@ -1,17 +1,18 @@
 import {SocialLogin} from '../../src/util/SocialLogin';
 import axios, {AxiosResponse} from 'axios';
 
-jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-const mockResponse: AxiosResponse<any> = {
+
+  jest.mock('axios');
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  const mockResponse: AxiosResponse<any> = {
     data: { id: '12345', nickname: 'Test User' },
     status: 200,
     statusText: 'OK',
     headers: {},
     config: {}
   } as AxiosResponse<any>;
-
 describe('SocialLogin 테스트', () => {
     let socialLogin: SocialLogin;
 
@@ -19,20 +20,24 @@ describe('SocialLogin 테스트', () => {
       socialLogin = new SocialLogin();
     });
 
-  describe('getKakaoData 함수 테스트', () => {
-    it('getKakaoData 함수 정상 처리', async () => {
+    describe('getKakaoData 함수 테스트', () => {
         const token = 'mock-token';
-        mockedAxios.get.mockResolvedValue(mockResponse);
-        const result = await socialLogin.getKakaoData(token);
-        expect(result).toEqual(undefined);
-        expect(mockedAxios).toHaveBeenCalledWith({
-            method: 'get',
-            url: 'https://kapi.kakao.com/v2/user/me',
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          }
-        )
+        it('getKakaoData 함수 정상 처리', async () => {
+            mockedAxios.get.mockResolvedValue(mockResponse)
+            const result = await socialLogin.getKakaoData(token);
+            expect(result).toEqual(mockResponse);
+            expect(mockedAxios.get).toHaveBeenCalledWith('https://kapi.kakao.com/v2/user/me', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        });
+
+        it('getKakaoData 함수 에러 처리', async () => {
+            const error = new Error('error');
+            mockedAxios.get.mockRejectedValue(error);
+            await expect(socialLogin.getKakaoData(token)).rejects.toThrow('error');
+        });
+
     });
-  });
 });
