f78122ac418bbee5537bbed5b97155b4a4d681dd
seungryeol <opop0421@naver.com>
2024-08-05 03:28:46 +0900

    [fix] s3Upload 변경

--- src/util/s3Upload.ts
+++ src/util/s3Upload.ts
@@ -3,25 +3,24 @@ import multerS3 from 'multer-s3';
 import { S3Client } from '@aws-sdk/client-s3';
 import { envs } from '../config/environment';
 
-
-
-
-export const uploadImage = multer({
+export const createUploadImage = (s3Client: S3Client) => multer({
   storage: multerS3({
-    s3: new S3Client({
-      credentials: {
-        accessKeyId: envs.s3.accessKey as string,
-        secretAccessKey: envs.s3.secretKey as string,
-      },
-      region: 'ap-northeast-2',
-    }),
+    s3: s3Client,
     bucket: 'eco-marketer',
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
-      cb(null, `images/${Date.now()}_${file.originalname} `);
+      cb(null, `images/${Date.now()}_${file.originalname}`);
     }
   })
 });
 
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: envs.s3.accessKey as string,
+    secretAccessKey: envs.s3.secretKey as string,
+  },
+  region: 'ap-northeast-2',
+});
 
+export const uploadImage = createUploadImage(s3Client);
