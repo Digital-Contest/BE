71708400f84ebb419478330be3c74775a988ec27
seungryeol <opop0421@naver.com>
2024-08-05 02:35:46 +0900

    [test] SocialLogin 완료

---
+++ test/util/SocialLogin.test.ts
@@ -0,0 +1,38 @@
+import {SocialLogin} from '../../src/util/SocialLogin';
+import axios, {AxiosResponse} from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const mockResponse: AxiosResponse<any> = {
+    data: { id: '12345', nickname: 'Test User' },
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {}
+  } as AxiosResponse<any>;
+
+describe('SocialLogin 테스트', () => {
+    let socialLogin: SocialLogin;
+
+    beforeEach(() => {
+      socialLogin = new SocialLogin();
+    });
+
+  describe('getKakaoData 함수 테스트', () => {
+    it('getKakaoData 함수 정상 처리', async () => {
+        const token = 'mock-token';
+        mockedAxios.get.mockResolvedValue(mockResponse);
+        const result = await socialLogin.getKakaoData(token);
+        expect(result).toEqual(undefined);
+        expect(mockedAxios).toHaveBeenCalledWith({
+            method: 'get',
+            url: 'https://kapi.kakao.com/v2/user/me',
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+    });
+  });
+});
